3e983d5e3e14870580e427f6e2619447aafc7f82
caffeine0coffee <kouta8145963@gmail.com>
2022-11-15 18:49:09 +0900

    todo: impl bvhExporter

---
+++ src/motionUtil/bvhExporter.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <filesystem>
+
+void exportLoopRangeToBvhFile(std::filesystem::path sourceFile,
+                              std::filesystem::path destFile,
+                              uint32_t loopStartIndex, uint32_t loopEndIndex) {
+    // TODO: immigrate from animator.cpp
+}
