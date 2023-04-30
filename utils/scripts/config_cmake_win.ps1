5b83b01e2344bbdfb246b0c4eea764629cc84daf
caffeine0coffee <kouta8145963@gmail.com>
2023-04-30 17:07:14 +0900

    update for windows build

--- utils/scripts/config_cmake_win.ps1
+++ utils/scripts/config_cmake_win.ps1
@@ -2,7 +2,6 @@ cmake `
 	-S . `
 	-B build `
 	-G Ninja `
-	-D BUILD_TYPE=Debug `
 	-DCMAKE_EXPORT_COMPILE_COMMANDS=YES `
 	-DCMAKE_C_COMPILER=clang `
-	-DCMAKE_CXX_COMPILER=clang++
+	-DCMAKE_CXX_COMPILER=clang++ `
