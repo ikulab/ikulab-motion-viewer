990b8a22356fea46737138670e1b7b9c086c6fb9
caffeine0coffee <dev@caffeinezero.info>
2024-07-30 01:48:16 +0900

    update workflow

--- utils/scripts/build_app_package.sh
+++ utils/scripts/build_app_package.sh
@@ -9,6 +9,9 @@ function green() {
 script_dir=$(readlink -f "$(dirname "$0")")
 project_dir=$(readlink -f "${script_dir}/../..")
 
+echo "script_dir: $script_dir"
+echo "project_dir: $project_dir"
+
 cd "$project_dir" || exit
 
 green "Building binary..."
