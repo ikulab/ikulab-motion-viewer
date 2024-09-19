351a6656b26d9a118257db63edca4ef988ce4057
caffeine0coffee <dev@caffeinezero.info>
2024-09-20 00:06:40 +0900

    test

--- test.sh
+++ test.sh
@@ -1,23 +1,3 @@
 #!/bin/bash
 
-echo "ls"
-
-ls
-
-echo "ls /c"
-
-ls /c
-
-ls "/c/Program Files"
-ls "/c\Program Files"
-ls "/c\\Program Files"
-ls "/c/Program\ Files"
-ls "/c\Program\ Files"
-ls "/c\\Program\ Files"
-ls "/c/Program Files/Microsoft Visual Studio"
-ls "/c/Program Files/Microsoft Visual Studio/2022"
-ls "/c/Program Files/Microsoft Visual Studio/2022/Enterprise"
-ls "/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC"
-ls "/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Redist"
-ls "/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Redist/MSVC"
-ls "/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Redist/MSVC/v143"
+ls "/c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Redist/MSVC" | grep -v "v"
