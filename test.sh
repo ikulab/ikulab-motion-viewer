e5dfa4a21286a81c5933a844a25e0a7da86775bc
caffeine0coffee <dev@caffeinezero.info>
2024-09-19 23:57:48 +0900

    test

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "ls"
+
+ls
+
+echo "ls /c"
+
+ls /c
+
+echo "ls c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Redist/MSVC/v143"
+ls "c/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Redist/MSVC/v143"
