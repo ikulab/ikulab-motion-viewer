76554610c1143dc2a7ce9fd7519932712c00c941
caffeine0coffee <dev@caffeinezero.info>
2024-12-22 03:00:08 +0900

    update: change macos startup program to ikulab-motion-viewer

--- assets/macos/startup.sh
+++ assets/macos/startup.sh
@@ -6,10 +6,8 @@ script_dir="$(dirname "$(realpath "${0}")")"
 binary_dir="${script_dir}"
 # ikulab-motion-viewer.app/Contentsのディレクトリ
 bundle_dir="$(realpath "${binary_dir}/../")"
-# Resourcesディレクトリ
-resources_dir="$bundle_dir/Resources"
 
 export VK_ICD_FILENAMES="${bundle_dir}/Resources/vulkan/icd.d/MoltenVK_icd.json"
 
 # ikulab-motion-viewerを起動
-"$binary_dir/imv_version_checker" "${resources_dir}/version_info" "${binary_dir}/ikulab-motion-viewer"
+"${binary_dir}/ikulab-motion-viewer"
