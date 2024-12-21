a1c88a61adc4c49f08fe37fb0dea566263d5670b
caffeine0coffee <dev@caffeinezero.info>
2024-12-21 15:07:46 +0900

    feat: launch version checker on startup on macos

--- assets/macos/startup.sh
+++ assets/macos/startup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # このスクリプトが格納されているディレクトリ
-script_dir="$(dirname "$(realpath "$0")")"
+script_dir="$(dirname "$(realpath "${0}")")"
 # ikulab-motion-viewerのバイナリが格納されているディレクトリ
-binary_dir="$script_dir"
-# ikulab-motion-viewer.appのディレクトリ
-bundle_dir="$(realpath $binary_dir/../)"
+binary_dir="${script_dir}"
+# ikulab-motion-viewer.app/Contentsのディレクトリ
+bundle_dir="$(realpath "${binary_dir}/../")"
+# Resourcesディレクトリ
+resources_dir="$bundle_dir/Resources"
 
 export VK_ICD_FILENAMES="${bundle_dir}/Resources/vulkan/icd.d/MoltenVK_icd.json"
 
 # ikulab-motion-viewerを起動
-"$binary_dir/ikulab-motion-viewer"
+"$binary_dir/imv_version_checker" "${resources_dir}/version_info" "${binary_dir}/ikulab-motion-viewer"
